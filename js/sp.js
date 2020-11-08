d5afb34c04c426f912637231c24560114e8271ce
Kirin0 <52816118+Kirin0@users.noreply.github.com>
2020-11-08 22:49:31 +0800

    Add files via upload

---
+++ js/sp.js
@@ -0,0 +1,66 @@
+
+var home = location.href,
+  s = $('#bgvideo')[0],
+  Siren = {
+    BSZ: function() {
+      $.getScript('//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js')
+    }, spause: function () {
+      $('#video-btn').addClass('video-play').removeClass('video-pause')
+      s.pause()
+    }, liveplay: function () {
+      if (s.oncanplay != undefined && $('.haslive').length > 0) {
+        if ($('.videolive').length > 0) {
+          Siren.splay()
+        }
+      }
+    }, livepause: function () {
+      if (s.oncanplay != undefined && $('.haslive').length > 0) {
+        Siren.spause()
+        $('.video-stu').css({
+          'bottom': '0px'
+        }).html('已暂停 ...')
+      }
+    }, addsource: function () {
+      $('.video-stu').html('正在载入视频 ...').css({
+        'bottom': '0px'
+      })
+      var t = Poi.movies.name.split(','),
+        _t = t[Math.floor(Math.random() * t.length)]
+      $('#bgvideo').attr('src', Poi.movies.url + '/' + _t)
+      $('#bgvideo').attr('video-name', _t)
+    }, LV: function () {
+      var _btn = $('#video-btn')
+      _btn.on('click', function () {
+        if ($(this).hasClass('loadvideo')) {
+          $(this).addClass('video-pause').removeClass('loadvideo').hide()
+          Siren.addsource()
+          s.oncanplay = function () {
+            Siren.splay()
+            $('#video-add').show()
+            _btn.addClass('videolive')
+            _btn.addClass('haslive')
+          }
+        } else {
+          if ($(this).hasClass('video-pause')) {
+            Siren.spause()
+            _btn.removeClass('videolive')
+            $('.video-stu').css({
+              'bottom': '0px'
+            }).html('已暂停 ...')
+          } else {
+            Siren.splay()
+            _btn.addClass('videolive')
+          }
+        }
+        s.onended = function () {
+          $('#bgvideo').attr('src', '')
+          $('#video-add').hide()
+          _btn.addClass('loadvideo').removeClass('video-pause')
+          _btn.removeClass('videolive')
+          _btn.removeClass('haslive')
+        }
+      })
+      $('#video-add').on('click', function () {
+        Siren.addsource()
+      })
+    }
